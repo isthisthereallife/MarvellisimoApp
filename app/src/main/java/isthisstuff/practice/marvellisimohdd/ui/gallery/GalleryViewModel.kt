fc43981970c14228d8cc3f6fb292cfd59314d783
Daniel <Daniel.falklimberg@gmail.com>
2020-09-08 15:23:31 +0200

    basic layout

--- app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/gallery/GalleryViewModel.kt
@@ -1,13 +1,13 @@
-package isthisstuff.practice.marvellisimohdd.ui.gallery
+package isthisstuff.practice.marvellisimohdd.ui.find
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class FindViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is find Fragment"
     }
     val text: LiveData<String> = _text
 }
