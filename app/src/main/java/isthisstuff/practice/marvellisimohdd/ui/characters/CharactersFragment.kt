e8db2eb58c35af461d5fc8de6357855a07b227b6
Daniel <Daniel.falklimberg@gmail.com>
2020-09-14 09:17:16 +0200

    by viewModels

--- app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/characters/CharactersFragment.kt
+++ app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/characters/CharactersFragment.kt
@@ -6,28 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import isthisstuff.practice.marvellisimohdd.R
 
 class CharactersFragment : Fragment() {
-    private lateinit var charactersViewModel: CharactersViewModel
-    lateinit var adapter:SearchResultsAdapter
+    private val charactersViewModel: CharactersViewModel by viewModels()
+    private var adapter:SearchResultsAdapter = SearchResultsAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        charactersViewModel = ViewModelProvider(this).get(CharactersViewModel::class.java)
         charactersViewModel.itemsList.observe(viewLifecycleOwner, Observer {
             adapter.data = it
         })
 
         val root = inflater.inflate(R.layout.fragment_characters, container, false)
 
-        adapter = SearchResultsAdapter()
         root.rootView.findViewById<RecyclerView>(R.id.search_results).adapter = adapter
         root.rootView.findViewById<ImageButton>(R.id.search_button).setOnClickListener {addItem()}
 
