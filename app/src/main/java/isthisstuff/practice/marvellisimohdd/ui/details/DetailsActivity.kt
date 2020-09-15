acb2397f96e5f809e1fd2f3a5158c74a03a100d4
Magnus <isthisthereallife@riseup.net>
2020-09-15 17:10:40 +0200

    added name to details page

--- app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/details/DetailsActivity.kt
+++ app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/details/DetailsActivity.kt
@@ -11,10 +11,12 @@ import isthisstuff.practice.marvellisimohdd.entities.MarvelObject
 
 class DetailsActivity : AppCompatActivity() {
 
-    private lateinit var star : ImageView
-    private lateinit var item:MarvelObject
-    private var thumbnail:String? = "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
-    private var info:String? = "There was a description here, it is gone now"
+    private lateinit var star: ImageView
+    private lateinit var item: MarvelObject
+    private var thumbnail: String? =
+        "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+    private var info: String? = "*NO DESCRIPTION AVAILABLE*"
+    private var name : String? = "Name goes here."
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +24,7 @@ class DetailsActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         star = findViewById(R.id.details_favstar)
-        star.setOnClickListener{ changeStar() }
+        star.setOnClickListener { changeStar() }
 
         /*
         val bundle:Bundle ?= intent.extras
@@ -33,10 +35,16 @@ class DetailsActivity : AppCompatActivity() {
 
         item = (intent.getSerializableExtra("item") as MarvelObject)
 
-        if(item.description.isNotBlank())
-        info = item.description.replace("ï¿½","'")
-        thumbnail = item.thumbnail.path+"."+item.thumbnail.extension
+        if (item.description.isNotBlank())
+            info = item.description.replace("ï¿½", "'")
+        thumbnail = item.thumbnail.path + "." + item.thumbnail.extension
 
+
+        if(item.name.isNotBlank())
+            name  = item.name.replace("ï¿½", "'")
+
+
+        findViewById<TextView>(R.id.details_name).text = name
         findViewById<TextView>(R.id.text_details).text = info
         Picasso.get().load(thumbnail).into(findViewById<ImageView>(R.id.imageView2))
     }
@@ -47,12 +55,12 @@ class DetailsActivity : AppCompatActivity() {
                 "Filled" -> {
                     star.setImageResource(R.drawable.ic_baseline_star_border_24)
                     star.setTag("Bordered")
-                    Toast.makeText(this,"Removed from favourites",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Removed from favourites", Toast.LENGTH_SHORT).show()
                 }
                 else -> {
                     star.setImageResource(R.drawable.ic_baseline_star_filled_24)
                     star.setTag("Filled")
-                    Toast.makeText(this,"Added to favourites",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Added to favourites", Toast.LENGTH_SHORT).show()
                 }
             }
         }
