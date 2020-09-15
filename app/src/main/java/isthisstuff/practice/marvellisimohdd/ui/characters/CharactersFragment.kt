e9cf5b71be18bb0734ad3ef7cea0e47f37fb8f3e
Magnus <isthisthereallife@riseup.net>
2020-09-15 11:53:18 +0200

    (parprogrammering) kommer åt ett specifikt element i sökresultat

--- app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/characters/CharactersFragment.kt
+++ app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/characters/CharactersFragment.kt
@@ -1,21 +1,17 @@
 package isthisstuff.practice.marvellisimohdd.ui.characters
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import isthisstuff.practice.marvellisimohdd.R
-import isthisstuff.practice.marvellisimohdd.retrofit.MARVEL_API_BASE_URL
-import isthisstuff.practice.marvellisimohdd.retrofit.MarvelService
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 
 class CharactersFragment : Fragment() {
     private val charactersViewModel: CharactersViewModel by viewModels()
@@ -34,7 +30,8 @@ class CharactersFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_characters, container, false)
 
         root.rootView.findViewById<RecyclerView>(R.id.search_results).adapter = adapter
-        root.rootView.findViewById<ImageButton>(R.id.search_button).setOnClickListener { performSearch() }
+        root.rootView.findViewById<ImageButton>(R.id.search_button)
+            .setOnClickListener { performSearch() }
 
         return root
     }
@@ -43,4 +40,6 @@ class CharactersFragment : Fragment() {
         charactersViewModel.getData()
         println(adapter.data)
     }
+
+
 }
--- app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/characters/SearchResultsAdapter.kt
+++ app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/characters/SearchResultsAdapter.kt
@@ -1,20 +1,32 @@
 package isthisstuff.practice.marvellisimohdd.ui.characters
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import isthisstuff.practice.marvellisimohdd.R
 import isthisstuff.practice.marvellisimohdd.MyViewHolder
 import isthisstuff.practice.marvellisimohdd.entities.MarvelCharacter
 
 class SearchResultsAdapter : RecyclerView.Adapter<MyViewHolder>() {
+    lateinit var itemNameView: TextView
+    lateinit var itemInfoView: TextView
+    lateinit var itemThumbnail: ImageView
     var data: List<MarvelCharacter> = mutableListOf()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
+
+    init {
+        //TODO lägg findViewById-grejjerna häri
+
+    }
 
     override fun getItemCount() = data.size
 
@@ -22,6 +34,19 @@ class SearchResultsAdapter : RecyclerView.Adapter<MyViewHolder>() {
         var item = data[position]
         holder.view.findViewById<TextView>(R.id.title_text).text = item.name
         holder.view.findViewById<TextView>(R.id.info_text).text = item.description
+        Picasso.get().load(item.thumbnail.path + "." + item.thumbnail.extension)
+            .into(holder.view.findViewById<ImageView>(R.id.search_results_image))
+        holder.view.findViewById<LinearLayout>(R.id.search_result_item)
+            .setOnClickListener { searchResultClicked(it, position) }
+    }
+
+    fun searchResultClicked(view: View, position: Int) {
+        Log.d(
+            "searchResultClicked",
+            "klickade på view: $view \n och this: $this \n och this.data:${this.data[position]}"
+        )
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -29,5 +54,16 @@ class SearchResultsAdapter : RecyclerView.Adapter<MyViewHolder>() {
         var view = layoutInflater.inflate(R.layout.search_item_view, parent, false) as LinearLayout
         return MyViewHolder(view)
     }
+/*
+    fun picassoImg(thumbnail: Thumbnail): Uri? {
+        return Picasso.get().load(thumbnail.path+thumbnail.extension).resize(50,50)
+            .centerCrop()
+            .into(R.id.search_results_image)
+            .setIndicatorsEnabled(true)
+
+    }
+
+*/
 }
+
 
