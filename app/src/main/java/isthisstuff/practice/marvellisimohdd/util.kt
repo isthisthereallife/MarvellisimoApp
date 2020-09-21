721ff733ea47a6f983efa440445a1e5cabb21c22
Magnus <isthisthereallife@riseup.net>
2020-09-21 10:19:13 +0200

    initial commit on activeUser

--- app/src/main/java/isthisstuff/practice/marvellisimohdd/util.kt
+++ app/src/main/java/isthisstuff/practice/marvellisimohdd/util.kt
@@ -28,3 +28,41 @@ fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
+
+/*
+// blame -> //M
+fun convertFromMarvelRealmObjectToMarvelObject(marvelRealmObject: MarvelRealmObject): MarvelObject {
+    val urls = mutableListOf<Urls>()
+    marvelRealmObject.urls?.forEach {
+        val urlsObject = Urls(type = it.type!!, url = it.url!!)
+        urls.add(urlsObject)
+    }
+    val marvelObject = MarvelObject(
+        id = marvelRealmObject.id!!, name = marvelRealmObject.name, title = marvelRealmObject.title,
+        description = marvelRealmObject.description, thumbnail = Thumbnail(
+            path = marvelRealmObject.thumbnail?.path!!,
+            extension = marvelRealmObject.thumbnail?.extension!!
+        ), urls = urls.toList()
+    )
+    return marvelObject
+}
+ */
+
+/*
+
+fun airplaneModeIsOn() : Boolean {
+    //AIRPLANE-MODE check
+    if (Settings.System.getInt(
+            context?.contentResolver,
+            Settings.Global.AIRPLANE_MODE_ON,
+            0
+        ) == 0
+    ) {
+        Log.d("Phone in airplane mode", "FALSE")
+        return false
+    } else {
+        Log.d("Phone is in airplane mode", "TRUE")
+        return true
+    }
+}
+ */
