b4647cfd497254500dee087ca994399a849eb290
Magnus <isthisthereallife@riseup.net>
2020-09-23 16:39:20 +0200

    Tagit bort ett obfuskerande fragment, kör direkt i aktivitet istället

--- app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/adapter/ActiveUsersAdapter.kt
+++ app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/adapter/ActiveUsersAdapter.kt
@@ -10,12 +10,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.FirebaseDatabase
 import isthisstuff.practice.marvellisimohdd.MyActiveUsersViewHolder
 import isthisstuff.practice.marvellisimohdd.R
+import isthisstuff.practice.marvellisimohdd.entities.MarvelObject
 import java.time.LocalDateTime
 
-class ActiveUsersAdapter() : RecyclerView.Adapter<MyActiveUsersViewHolder>() {
-    lateinit var itemEmail: TextView
+class ActiveUsersAdapter(_sender: String, _marvelObject: MarvelObject) :
+    RecyclerView.Adapter<MyActiveUsersViewHolder>() {
     private var database = FirebaseDatabase.getInstance()
-    private var databaseMessageReference = database.getReference("messages")
+    var sender = _sender
+    var marvelObject = _marvelObject
 
     var data = listOf<Pair<String, String>>()
         set(value) {
@@ -23,11 +25,9 @@ class ActiveUsersAdapter() : RecyclerView.Adapter<MyActiveUsersViewHolder>() {
             notifyDataSetChanged()
         }
 
-
     override fun getItemCount() = data.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyActiveUsersViewHolder {
-        Log.d("ActiveUsersAdapter->onCreateViewHolder", "Wow, en ViewHolder!")
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater.inflate(R.layout.item_active_user, parent, false) as LinearLayout
         return MyActiveUsersViewHolder(view)
@@ -35,32 +35,29 @@ class ActiveUsersAdapter() : RecyclerView.Adapter<MyActiveUsersViewHolder>() {
 
     override fun onBindViewHolder(holder: MyActiveUsersViewHolder, position: Int) {
         val item = data[position]
-        Log.d(
-            "HÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄR !!!!!!!!!! ¤#/&¤/#(&¤/ ActiveUsersAdapter->onBindViewHolder",
-            item.second.toString()
-        )
-
-        Log.d("ActiveUsersAdapter->onBindViewHolder", "Wow, ett element! $item")
-
         holder.view.findViewById<TextView>(R.id.user_email_text_view).text =
             item.second.replace(",", ".")
         holder.view.findViewById<LinearLayout>(R.id.active_user_item).setOnClickListener {
-
-            //        val myReference = database.getReference(message)**/
-            val timeString = LocalDateTime.now().toString().replace(".", ":")
-
-            val message =
-                "SENDER<${"TODO put sender here "}>RECEIVER<${item.second}>TIME<$timeString>PAYLOAD:SERIES_ID<Todo skicka med ett id>"
-
-            databaseMessageReference.push().setValue(message)
-
-
+            sendMessage(
+                sender = sender,
+                receiver = item.second,
+                payload = marvelObject.id.toString()
+            )
             val updatedText = "Skickat till ${item.second}"
             holder.view.findViewById<TextView>(R.id.user_email_text_view).text = updatedText
-
-            //och kanske göra en toast om att det skickats
-
         }
+
+    }
+
+    private fun sendMessage(sender: String, receiver: String, payload: String) {
+        val customDatabaseMessageReference =
+            database.getReference("<TO:${receiver.replace(".", ",")}>")
+        val timeString = LocalDateTime.now().toString().replace(".", ":")
+
+        val message = "<SENDER>$sender</SENDER><PAYLOAD>$payload</PAYLOAD><TIMESTAMP>$timeString</TIMESTAMP>"
+
+        customDatabaseMessageReference.push().setValue(message)
+
 
     }
 
