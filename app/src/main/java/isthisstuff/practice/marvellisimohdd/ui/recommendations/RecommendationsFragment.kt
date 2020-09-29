c8d6cef7dc4f1b68c519ef0035138f282829a12d
Magnus <isthisthereallife@riseup.net>
2020-09-29 09:41:25 +0200

    använder samma ram för item i rekommendations som i search

--- app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/recommendations/RecommendationsFragment.kt
+++ app/src/main/java/isthisstuff/practice/marvellisimohdd/ui/recommendations/RecommendationsFragment.kt
@@ -1,31 +1,31 @@
 package isthisstuff.practice.marvellisimohdd.ui.recommendations
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import isthisstuff.practice.marvellisimohdd.R
 import isthisstuff.practice.marvellisimohdd.entities.MarvelObject
 import isthisstuff.practice.marvellisimohdd.entities.Thumbnail
 import isthisstuff.practice.marvellisimohdd.entities.Urls
 import isthisstuff.practice.marvellisimohdd.ui.data.MarvelDatatypes
-import isthisstuff.practice.marvellisimohdd.ui.data.MarvelViewModel
 import isthisstuff.practice.marvellisimohdd.ui.details.DetailsActivity
 
 class RecommendationsFragment : Fragment() {
@@ -38,12 +38,8 @@ class RecommendationsFragment : Fragment() {
     private var activeUserMessagesReference = database.getReference("<TO:$currentUserNoDots>")
     private var detachedView = false
 
-    //private var activeUserMessagesReference = database.getReference("currentUsers")
     lateinit var root: View
 
-
-    val marvelViewModel: MarvelViewModel by viewModels()
-    private var messagesList = listOf<Pair<String, String>>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -68,7 +64,6 @@ class RecommendationsFragment : Fragment() {
                         concurrentMessagesHashMap = messages as HashMap<String, String>
                         printMessages(concurrentMessagesHashMap.toList())
                     }
-
                 }
 
                 override fun onCancelled(databaseError: DatabaseError) {
@@ -81,7 +76,7 @@ class RecommendationsFragment : Fragment() {
             }
             Log.d("Inflating fragment", "ATTACHING VALUE EVENT LISTENER")
             activeUserMessagesReference.addValueEventListener(messageListener)
-        } else Toast.makeText(this.context, "Log in to use this cool feature", Toast.LENGTH_SHORT)
+        } else Toast.makeText(this.context, getText(R.string.toast_log_in_prompt), Toast.LENGTH_SHORT)
             .show()
         return root
     }
@@ -91,47 +86,38 @@ class RecommendationsFragment : Fragment() {
         Log.d("onDetach", "Detaching...")
         detachedView = true
 
-        //    private var activeUserMessagesReference = database.getReference("<TO:$currentUserNoDots>")
-    }
+      }
 
 
     fun printMessages(toList: List<Pair<String, String>>) {
         toList.forEach { it ->
-            val textView = TextView(context)
-            textView.textSize = 15f
-            val sender = it.second.substringAfter("<SENDER>").substringBefore("</SENDER>")
+            val sender = "Recommended by "+it.second.substringAfter("<SENDER>").substringBefore("</SENDER>")
             val type = it.second.substringAfter("<MARVELTYPE>").substringBefore("</MARVELTYPE>")
             val name = it.second.substringAfter("<MARVELOBJECTNAME>")
                 .substringBefore("</MARVELOBJECTNAME>")
             val payload = it.second.substringAfter("<PAYLOAD>").substringBefore("</PAYLOAD")
-            textView.setTextColor(resources.getColor(R.color.secondaryTextColor))
 
-            val text = "$sender thinks you should search for the $type $name \n"
-            Log.d("printMessages", "SKRIVER UT EN TEXTVY")
-
-            textView.text = text
-            target.addView(textView)
-
-            //pallar inte göra MarvelObject parcelable. detta är en lösning. deal with it.
             val marvelObject = makeMarvelObjectFromPayload(payload, type)
+
+            val view =  inflate(this.context,R.layout.search_item_view,null)
+            val img = view.findViewById<ImageView>(R.id.search_results_image)
+            Picasso.get().load(marvelObject.thumbnail.path+"."+marvelObject.thumbnail.extension).into(img)
+            val innerConstraint = view.findViewById<ConstraintLayout>(R.id.search_result_inner_constraint)
+            innerConstraint.findViewById<TextView>(R.id.title_text).text = name
+            innerConstraint.findViewById<TextView>(R.id.info_text).text = sender
+            val xImage = innerConstraint.findViewById<ImageView>(R.id.search_favstar)
+            xImage.setImageResource(R.drawable.ic_baseline_x_24)
+            target.addView(view)
             val intent = Intent(this.context, DetailsActivity::class.java)
             intent.putExtra("item", marvelObject)
-            textView.setOnClickListener { startActivityForResult(intent, 2) }
-        }
+            view.setOnClickListener { startActivityForResult(intent, 2) }
+            xImage.setOnClickListener {
 
-        /* //GAMMLA LÖSNINGEN
-        val recoTextView = root.findViewById<TextView>(R.id.recommendations_text_view)
-        var textBuilder = ""
-        toList.forEach { it ->
-            Log.d("Loopar meddelanden att skriva ut", it.second)
-            val sender = it.second.substringAfter("<SENDER>").substringBefore("</SENDER>")
-            val type = it.second.substringAfter("<MARVELTYPE>").substringBefore("</MARVELTYPE>")
-            val name = it.second.substringAfter("<MARVELOBJECTNAME>").substringBefore("</MARVELOBJECTNAME>")
+                target.removeView(view)
+                //TODO ta bort från databasen också
 
-            textBuilder = textBuilder.plus("$sender thinks you should search for the $type $name").plus("\n\n")
+                }
         }
-        recoTextView.text = textBuilder
-        */
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -157,7 +143,7 @@ class RecommendationsFragment : Fragment() {
         }
     }
 
-    fun makeMarvelObjectFromPayload(marvelString: String, type: String): MarvelObject {
+    private fun makeMarvelObjectFromPayload(marvelString: String, type: String): MarvelObject {
         val id = marvelString.substringAfter("MarvelObject(id=").substringBefore(", name=")
         val name = if (type.equals("character")) marvelString.substringAfter(", name=")
             .substringBefore(", title=") else null
@@ -165,7 +151,6 @@ class RecommendationsFragment : Fragment() {
             .substringBefore(", urls=") else null
         val urlType = marvelString.substringAfter("urls=[Urls(type=").substringBefore(", url=")
         val url = marvelString.substringAfter(", url=").substringBefore(",")
-        Log.d("fan osäker på denna url asså", url)
         val urls = Urls(type = urlType, url = url)
         val description = marvelString.substringAfter(", description=").substringBefore(",")
         val path = marvelString.substringAfter(", thumbnail=Thumbnail(path=")
@@ -173,11 +158,6 @@ class RecommendationsFragment : Fragment() {
         val extension = marvelString.substringAfter(", extension=").substringBefore(")")
         val thumbnail = Thumbnail(path = path, extension = extension)
 
-
-        Log.d("MARVELSTRING", marvelString)
-        val marvelObject =
-            MarvelObject(id.toInt(), name, title, listOf(urls), description, thumbnail)
-        Log.d("marvelObject.toString()", marvelObject.toString())
-        return marvelObject
+        return MarvelObject(id.toInt(), name, title, listOf(urls), description, thumbnail)
     }
 }
